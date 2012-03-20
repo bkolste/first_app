d490e446b74565759acac3a36623e4c0e844610a
Bruce Kolste <bkolste@middil.com>
2012-03-20 11:23:46 -0600

    Finish layout and routes

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    @title = "Home"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+
+  def about
+    @title = "About"
+  end
+
+  def help
+    @title = "Help"
+  end
+  
+end
+
